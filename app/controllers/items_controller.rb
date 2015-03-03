ca208de68517fe7a952300459c55bddef49cf5b8
Robert Hutchinson <robert.hutchinson@gmail.com>
2015-03-03 15:46:19 -0500

    functioning edits

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,13 +5,29 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @list =List.find params[:list_id]
     item_params = params[:item]
-    @item = Item.new item_name: item_params["item_name"].downcase.capitalize, list_id: params[:id]
+    @item = Item.new item_name: item_params["item_name"].downcase.capitalize, list_id: params[:list_id]
     if @item.save
-      redirect_to list_path(params[:id])
+      redirect_to list_path(params[:list_id])
     else
       render :new
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update 
+    item_params = params[:item]
+    @item = Item.find params[:id]
+    if params[:done] == "1"
+      @item.update done: true
+    else
+      @item.update done:false
+    end
+    @item.update(due_date: item_params[:due_date])
+    redirect_to list_path(@item.list_id)
+  end
 end
